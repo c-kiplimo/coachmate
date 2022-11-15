e830a846c17711328432f1b743f679d083db6eb9
davidnene <davienesh4@gmail.com>
2022-11-15 13:36:45 +0300

    Configure POST request

--- the-couch-api/src/main/java/com/natujenge/thecouch/web/rest/ClientResource.java
+++ the-couch-api/src/main/java/com/natujenge/thecouch/web/rest/ClientResource.java
@@ -1,12 +1,16 @@
 package com.natujenge.thecouch.web.rest;
 
+import com.natujenge.thecouch.domain.Client;
 import com.natujenge.thecouch.service.ClientService;
+import com.natujenge.thecouch.service.UserService;
+import lombok.Cleanup;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -15,9 +19,26 @@ public class ClientResource {
 
     @Autowired
     ClientService clientService;
-    
+
+
+    @GetMapping
     public ResponseEntity<?> getClients(){
         try{
+            List<Client> clientList = clientService.viewClients();
+
+            return new ResponseEntity(clientList, HttpStatus.OK);
+        } catch(Exception e) {
+            log.error(".....", e);
+            return new ResponseEntity( "Error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @PostMapping
+    public ResponseEntity<?> createClient(@RequestBody Client client){
+        try{
+            log.info("Client request received {)", client);
+
+            Client clientResponse = clientService.createClient(client);
+            return new ResponseEntity(clientResponse, HttpStatus.CREATED);
 
         } catch(Exception e) {
             log.error(".....", e);
