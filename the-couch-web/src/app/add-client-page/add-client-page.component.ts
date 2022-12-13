7e380b7efe142af3e84627e90c8feb73b79be68f
StevoM <60152814+stevomusembi@users.noreply.github.com>
2022-12-13 14:00:04 +0300

    consumed add client API

--- the-couch-web/src/app/add-client-page/add-client-page.component.ts
+++ the-couch-web/src/app/add-client-page/add-client-page.component.ts
@@ -6,37 +6,42 @@ import { ClientService } from '../services/ClientService';
 @Component({
   selector: 'app-add-client-page',
   templateUrl: './add-client-page.component.html',
-  styleUrls: ['./add-client-page.component.css']
+  styleUrls: ['./add-client-page.component.css'],
 })
 export class AddClientPageComponent implements OnInit {
-addClient!: FormGroup;
-firstName: any;
-lastName: any;
-  constructor(private ClientService: ClientService,  private router: Router,
-    private formbuilder: FormBuilder, ) { }
+  addClient!: FormGroup;
+  firstName: any;
+  lastName: any;
+  constructor(
+    private ClientService: ClientService,
+    private router: Router,
+    private formbuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
-    this.addClient= this.formbuilder.group({
-      firstName: '',
-      lastName: '',
-      msisdn: '',
-      email: '',
-      physicalAddress: '',
+    this.addClient = this.formbuilder.group({
+      name: ' ',
+      type: ' ',
+      msisdn: ' ',
+      email_address: ' ',
+      physical_address: ' ',
+      profession: ' ',
+      payment_mode: ' ',
+      status: 'ACTIVE',
+      reason: '',
+      createdBy: ' Amos',
+      lastUpdatedBy: ' Amos',
     });
-    this.newClient();
   }
 
-  Client = {}
+  // Client = {};
   newClient() {
-    console.log(this.addClient.value);
-    this.addClient;
-      (this.addClient.value)
-      .subscribe((response: any) => {
+    console.log("add client form=>", this.addClient.value);
+    this.ClientService.addClient(this.addClient.value).subscribe(
+      (response: any) => {
         console.log(response);
         this.router.navigate(['/clients']);
-      });
+      }
+    );
   }
-
 }
-
-
