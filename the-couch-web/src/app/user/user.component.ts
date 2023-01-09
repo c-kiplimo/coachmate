3a4a86fa7a5f84439ca8271ea1933efe6fe43b26
amos kinuthia <amoskinuthia15@gmail.com>
2023-01-09 16:59:43 +0300

    added logout

--- the-couch-web/src/app/user/user.component.ts
+++ the-couch-web/src/app/user/user.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  faRightFromBracket,
+  faSign,
+  faUser,
+  faUsers,
+} from '@fortawesome/free-solid-svg-icons';
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  logoutIcon = faRightFromBracket;
+  loginIcon = faSign;
+  userIcon = faUser;
+  customersIcon = faUsers;
+  businessName: any;
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   User: any
   ngOnInit(): void {
@@ -16,6 +27,12 @@ export class UserComponent implements OnInit {
       
     console.log(this.User.coach.businessName) 
   }
-   
+  logOut() {
+    sessionStorage.removeItem('token');
+    sessionStorage.removeItem('businessName');
+    sessionStorage.removeItem('user');
+    this.businessName = '';
 
+  }
+  
 }
