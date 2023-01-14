79ab473ce8d87d04ab8413c82b71e96e2bb05ab8
Gilbert kitetu <gilbertkitetu2@gmail.com>
2023-01-14 13:43:53 +0300

    add client with created by and coach id

--- the-couch-web/src/app/clients/clients.component.ts
+++ the-couch-web/src/app/clients/clients.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ClientService } from '../services/ClientService';
 import { Router } from '@angular/router';
 import { style, animate, transition, trigger } from '@angular/animations';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-clients',
@@ -24,6 +25,8 @@ export class ClientsComponent implements OnInit {
     status: '',
     searchItem: '',
   };
+
+  updateClient!: FormGroup;
 
   constructor(private ClientService: ClientService, private router: Router) { }
   
@@ -60,7 +63,27 @@ export class ClientsComponent implements OnInit {
 
 
   }
-  deleteClient(id:any){
-    this.deleteClient;
+  editClient(client:any){
+    this.ClientService.editClient(client).subscribe(
+      (response) => {
+        this.getClients();
+        this.loading = false;
+
+      }, (error) => {
+        console.log(error)
+      }
+    );
+  }
+
+  suspendClient(client:any){
+    this.ClientService.suspendClient(client).subscribe(
+      (response) => {
+        this.getClients();
+        this.loading = false;
+
+      }, (error) => {
+        console.log(error)
+      }
+    );
   }
 }
