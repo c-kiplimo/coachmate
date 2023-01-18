26fdfc4c4d408ca2d01b7682247ac873d6475f4e
amos kinuthia <amoskinuthia15@gmail.com>
2023-01-18 16:24:26 +0300

    did the feeback form

--- the-couch-web/src/app/feeback-form/feeback-form.component.ts
+++ the-couch-web/src/app/feeback-form/feeback-form.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-feeback-form',
   templateUrl: './feeback-form.component.html',
   styleUrls: ['./feeback-form.component.css']
 })
 export class FeebackFormComponent implements OnInit {
+  feedbackForm = new FormGroup({
+    goalSetting: new FormControl(''),
+    emotionManagement: new FormControl(''),
+    activeListening: new FormControl(''),
+    clarification: new FormControl(''),
+    agenda: new FormControl(''),
+    presence: new FormControl(''),
+    additionalComments: new FormControl(''),
+  });
+
+  submitForm() {
+    // send data to backend for future use
+  }
 
   constructor() { }
 
