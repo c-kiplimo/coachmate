e0552335acd44d8a21b67230c853ea368e122589
kiplimo-dev <kiplimocollins855@gmail.com>
2023-02-18 10:51:38 -0600

    added notification template

---
+++ the-couch-api/src/main/java/com/natujenge/thecouch/domain/NotificationTemplate.java
@@ -0,0 +1,38 @@
+package com.natujenge.thecouch.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import com.natujenge.thecouch.domain.enums.SessionTemplateType;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tbl_notification_templates")
+@Entity
+public class NotificationTemplate {
+    @SequenceGenerator(
+            name = "notification_template_sequence",
+            sequenceName = "notification_template_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "notification_template_sequence")
+    @Id
+    private Long id;
+    private SessionTemplateType sessionTemplateType;
+    private boolean enable = true;
+    private String email;
+    private String sms;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+    @Column(nullable = false)
+    private String createdBy;
+    private LocalDateTime lastUpdatedAt;
+    private String lastUpdatedBy;
+}
