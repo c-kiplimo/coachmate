c67b1ef625bf9f8cd91be1e3ecb367f9b8affdcd
Amoskinuthia <Amoskinuthia15@gmail.com>
2023-02-19 09:00:09 +0300

    session notifications

--- the-couch-api/src/main/java/com/natujenge/thecouch/repository/SessionRepository.java
+++ the-couch-api/src/main/java/com/natujenge/thecouch/repository/SessionRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 @Repository
@@ -36,4 +37,6 @@ public interface SessionRepository extends PagingAndSortingRepository<Session,Lo
     List<Session> findSessionByOrgId(Long orgId);
 
     Optional<Session> getSessionById(Long id);
+
+    List<Session> findSessionBySessionDate(LocalDate now);
 }
