1f4c7c207f114fb7cd6417fb590e81359cc09fba
Amoskinuthia <Amoskinuthia15@gmail.com>
2023-02-20 08:14:11 +0300

    Coach Availability entity

--- the-couch-api/src/main/java/com/natujenge/thecouch/domain/CoachAvailability.java
+++ the-couch-api/src/main/java/com/natujenge/thecouch/domain/CoachAvailability.java
@@ -1,2 +1,23 @@
-package com.natujenge.thecouch.domain;public class CoachAvailability {
+package com.natujenge.thecouch.domain;
+
+import lombok.Data;
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "coach_schedules")
+@Data
+public class CoachAvailability {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String dayOfWeek;
+
+    @ElementCollection
+    @CollectionTable(name = "schedule_slots")
+    private List<DaySchedule> slots;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Coach coach;
+
 }
