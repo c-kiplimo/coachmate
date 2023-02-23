682ae7502cf4ac868aaec0f2e29ccefacb067c62
Boniface <muteaboniface@gmail.com>
2023-02-23 10:24:17 +0300

    billing account repo and wallet to qdsl

--- the-couch-api/src/main/java/com/natujenge/thecouch/repository/ClientBillingAccountRepository.java
+++ the-couch-api/src/main/java/com/natujenge/thecouch/repository/ClientBillingAccountRepository.java
@@ -4,12 +4,15 @@ import com.natujenge.thecouch.domain.ClientBillingAccount;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface ClientBillingAccountRepository extends JpaRepository<ClientBillingAccount,Long> {
+public interface ClientBillingAccountRepository extends PagingAndSortingRepository<ClientBillingAccount,Long>,
+        QuerydslPredicateExecutor<ClientBillingAccount> {
 
     Optional<ClientBillingAccount> findFirstByCoachIdAndClientIdOrderByIdDesc(Long coachId,Long clientId);
 
