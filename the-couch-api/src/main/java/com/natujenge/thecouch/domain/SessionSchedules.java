ba1165ba94f51da6854c1854ff7df3964e1e4977
Gilbert kitetu <gilbertkitetu2@gmail.com>
2023-03-10 09:12:58 +0300

    add coach avalaible slot apis

--- the-couch-api/src/main/java/com/natujenge/thecouch/domain/SessionSchedules.java
+++ the-couch-api/src/main/java/com/natujenge/thecouch/domain/SessionSchedules.java
@@ -1,2 +1,29 @@
-package com.natujenge.thecouch.domain;public class SessionSchedules {
+package com.natujenge.thecouch.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tbl_sessionSchedules")
+public class SessionSchedules {
+    @GeneratedValue (strategy = GenerationType.AUTO.IDENTITY)
+    @Id
+    private Long id;
+    private Long orgId;
+
+    private LocalDate sessionDate;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    @ManyToOne
+    @JoinColumn(name = "coach_id")
+    Coach coach;
 }
---
+++ the-couch-api/src/main/java/com/natujenge/thecouch/service/SessionSchedulesService.java
@@ -0,0 +1,61 @@
+package com.natujenge.thecouch.service;
+
+import com.natujenge.thecouch.domain.Coach;
+import com.natujenge.thecouch.domain.SessionSchedules;
+import com.natujenge.thecouch.repository.CoachRepository;
+import com.natujenge.thecouch.repository.SessionSchedulesRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
+@Slf4j
+@Transactional
+
+public class SessionSchedulesService {
+    @Autowired
+    SessionSchedulesRepository sessionSchedulesRepository;
+    CoachRepository coachRepository;
+
+    //Get all schedule by caoch id
+    public SessionSchedules findSessionSchedulesByCoachId(Long id) {
+        log.debug("Request to get SessionSchedules : {} by coach id {}", id);
+
+        Optional<SessionSchedules> sessionSchedules = sessionSchedulesRepository.findAllByCoachId(id);
+
+        if (sessionSchedules.isPresent()){
+            return sessionSchedules.get();
+        } else {
+            throw new IllegalStateException("Session schedules not founs!");
+
+        }
+
+    }
+
+    //Create session schedule
+    public SessionSchedules createSessionSchedule(Long coachId, SessionSchedules sessionSchedules) throws IllegalArgumentException{
+        log.info("Creating a new session schedule");
+        Optional<Coach> optionalCoach = coachRepository.findCoachById(coachId);
+
+        if(optionalCoach.isEmpty()){
+            log.warn("Coach with id {} not found", coachId );
+            throw new IllegalArgumentException("Coach not found");
+        }
+
+        sessionSchedules.setSessionDate(sessionSchedules.getSessionDate());
+        sessionSchedules.setCoach(optionalCoach.get());
+        sessionSchedules.setEndTime(sessionSchedules.getEndTime());
+        sessionSchedules.setStartTime(sessionSchedules.getStartTime());
+        sessionSchedules.setOrgId(sessionSchedules.getOrgId());
+
+        try {
+            return sessionSchedulesRepository.save(sessionSchedules);
+        }catch (Exception e) {
+            log.error("Error occurred ", e);
+            throw new IllegalArgumentException(e.getMessage());
+        }
+    }
+}
