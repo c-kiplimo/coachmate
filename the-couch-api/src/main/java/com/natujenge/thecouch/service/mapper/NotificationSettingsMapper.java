1a32f5c0888122df202a207c414a7bb31d78f792
Gilbert kitetu <gilbertkitetu2@gmail.com>
2023-04-23 16:51:24 +0300

    resolve

--- the-couch-api/src/main/java/com/natujenge/thecouch/service/mapper/NotificationSettingsMapper.java
+++ the-couch-api/src/main/java/com/natujenge/thecouch/service/mapper/NotificationSettingsMapper.java
@@ -3,6 +3,7 @@ package com.natujenge.thecouch.service.mapper;
 
 import com.natujenge.thecouch.service.dto.NotificationSettingsDTO;
 import com.natujenge.thecouch.domain.NotificationSettings;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
